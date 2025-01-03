bb5ef99c2fa11860c574d36f13ded982c01a1621
HuyiCui <cuihuyi512@gmail.com>
2025-01-03 22:09:36 +0000

    Nowplaying movies from TMDB

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -46,4 +46,20 @@ export const getMovieGenres = async () => {
     } catch (error) {
         throw error;
     }
-};
+  };
+
+  export const getNowPlayingMovies = async () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    ).then((response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
